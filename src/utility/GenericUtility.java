76501eeb362644cd947a3b311be272135111f6ee
Anwoy <Anwoy@DESKTOP-EK0RPPP>
2018-04-18 19:30:53 +0530

    2nd User first commit

--- src/utility/GenericUtility.java
+++ src/utility/GenericUtility.java
@@ -4,7 +4,7 @@ public class GenericUtility {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is a git project");
+		System.out.println("This is a git project only");
 
 	}
 
