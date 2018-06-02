8af8674f70ed579fb4d46c25a5f4b2eb9fca1821
Anwoy <Anwoy@DESKTOP-EK0RPPP>
2018-06-02 11:04:24 +0530

    update generic utility

--- src/utility/GenericUtility.java
+++ src/utility/GenericUtility.java
@@ -1,11 +1,26 @@
 package utility;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class GenericUtility {
+	
+	WebDriver driver;
 
 	public static void main(String[] args) {
 		
 		System.out.println("This is the git project only");
 
+	}
+	
+	@Test
+	public void printName()
+	{
+		//System.setProperty("webdriver.gecko.driver", "geckodriver");
+		
+		driver = new FirefoxDriver();
+		driver.navigate().to("https://www.google.com/");
 	}
 
 }
